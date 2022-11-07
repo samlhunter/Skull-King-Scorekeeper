13b1e7bab7abc3fa7809ae5496aef502887b121e
Ignacio R. de Almeida <57123713+IgnacioAlmeida@users.noreply.github.com>
2022-11-06 19:50:37 -0700

    Adjust to show background img

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const COLOR_BROWN = Color.fromRGBO(64,59,34,1.0);
+const COLOR_LIGHT_BROWN = Color.fromRGBO(250, 237, 205, 1.0);
+const COLOR_YELLOW = Color.fromRGBO(228, 193, 77, 1.0);
+const COLOR_GREY = Color.fromRGBO(221, 221, 221, 1.0);
--- lib/views/initialScreen.view.dart
+++ lib/views/initialScreen.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skull_king_scorekeeper/utils/constants.dart';
 import 'package:skull_king_scorekeeper/views/createGame.view.dart';
 
 class InitialScreenView extends StatefulWidget {
@@ -10,48 +11,59 @@ class _InitialScreenViewState extends State<InitialScreenView> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('background.png'),
-            fit: BoxFit.cover,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.png'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Scaffold(
-            appBar: AppBar(title: const Text('Start')),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  'Welcome to Skull King Scorekeeper!',
-                  style: TextStyle(fontSize: 50),
-                  textAlign: TextAlign.center,
-                ),
-                Spacer(),
-                const Text(
-                  'Here you can create a new game or resume a previous one!',
-                  style: TextStyle(fontSize: 30),
-                  textAlign: TextAlign.center,
-                ),
-                Spacer(
-                  flex: 3,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return CreateGameView();
-                      }));
-                    },
-                    child: const Text('Create New Game')),
-                const OutlinedButton(
-                    onPressed: null,
-                    child: Text(
-                        'Continue Previous Game')), // null onPressed == disabled button
-                Spacer()
-              ],
-            )),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+              body: Center(
+                child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Spacer(
+                        flex:2
+                    ),
+                    const Text(
+                      'Welcome to Skull King Scorekeeper!',
+                      style: TextStyle(fontSize: 40),
+                      textAlign: TextAlign.center,
+                    ),
+                    Spacer(),
+                    const Text(
+                      'Here you can create a new game or resume a previous one!',
+                      style: TextStyle(fontSize: 25),
+                      textAlign: TextAlign.center,
+                    ),
+                    Spacer(),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: COLOR_LIGHT_BROWN,
+                            onPrimary: Colors.black87,
+                            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                            textStyle: TextStyle(
+                                fontSize: 15)),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                                return CreateGameView();
+                              }));
+                        },
+                        child: const Text('Create New Game')),
+                    OutlinedButton(
+                        onPressed: null,
+                        child: Text(
+                            'Continue Previous Game')), // null onPressed == disabled button
+                    Spacer()
+                  ],
+                )),
+              )
+        )
       ),
     );
   }
