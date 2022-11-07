c6964cd2d7c6de354f2db551b83a70499ef20b0f
Ignacio R. de Almeida <57123713+IgnacioAlmeida@users.noreply.github.com>
2022-11-06 21:49:58 -0700

    add style to betting view

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,5 @@ const COLOR_BROWN = Color.fromRGBO(64,59,34,1.0);
 const COLOR_LIGHT_BROWN = Color.fromRGBO(250, 237, 205, 1.0);
 const COLOR_YELLOW = Color.fromRGBO(228, 193, 77, 1.0);
 const COLOR_GREY = Color.fromRGBO(221, 221, 221, 1.0);
+
+//TODO add fonts here kind of as shown in https://www.youtube.com/watch?v=Y6xAQVXkhLA
--- lib/views/betting.view.dart
+++ lib/views/betting.view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:skull_king_scorekeeper/components/FeaturesDrawer.component.dart';
+import 'package:skull_king_scorekeeper/utils/buttons.dart';
 import 'package:skull_king_scorekeeper/views/enterResults.view.dart';
 
 class BettingView extends StatefulWidget {
@@ -25,78 +26,93 @@ class _BettingViewState extends State<BettingView> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text('Round $roundNumber - Bets')),
+          appBar: AppBar(
+              title: Text('Round $roundNumber - Bets'),
+          ),
           drawer: generateGameDrawer(context),
-          body: Column(
-            children: [
-              const Text(
-                'Place Yer Bets',
-                style: TextStyle(fontSize: 30),
+          body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background.png'),
+                fit: BoxFit.cover,
               ),
-              Expanded(
-                  child: ListView.separated(
-                padding: const EdgeInsets.all(8),
-                itemCount: players.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    height: 50,
-                    color: Colors.amber[colorCodes[index]],
-                    child: Row(children: [
-                      Text('Player:  ${players[index]}'),
-                      Spacer(),
-                      Text('Bet:  ${bets[index]}'),
-                      Ink(
-                        decoration: const ShapeDecoration(
-                          color: Colors.lightBlue,
-                          shape: CircleBorder(),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(Icons.add_circle_outline),
-                          color: Colors.white,
-                          onPressed: () {
-                            setState(() {
-                              if (bets[index] < roundNumber) {
-                                bets[index] += 1;
-                              }
-                            });
-                          },
-                        ),
+            ),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+                body: Center(
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Place Yer Bets',
+                        style: TextStyle(fontSize: 30),
                       ),
-                      Ink(
-                        decoration: const ShapeDecoration(
-                          color: Colors.lightBlue,
-                          shape: CircleBorder(),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(Icons.remove_circle_outline),
-                          color: Colors.white,
+                      Expanded(
+                          child: ListView.separated(
+                            padding: const EdgeInsets.all(8),
+                            itemCount: players.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(
+                                height: 50,
+                                color: Colors.amber[colorCodes[index]],
+                                child: Row(children: [
+                                  Text('Player:  ${players[index]}'),
+                                  Spacer(),
+                                  Text('Bet:  ${bets[index]}'),
+                                  Ink(
+                                    decoration: const ShapeDecoration(
+                                      color: Colors.lightBlue,
+                                      shape: CircleBorder(),
+                                    ),
+                                    child: IconButton(
+                                      icon: const Icon(Icons.add_circle_outline),
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        setState(() {
+                                          if (bets[index] < roundNumber) {
+                                            bets[index] += 1;
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  Ink(
+                                    decoration: const ShapeDecoration(
+                                      color: Colors.lightBlue,
+                                      shape: CircleBorder(),
+                                    ),
+                                    child: IconButton(
+                                      icon: const Icon(Icons.remove_circle_outline),
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        setState(() {
+                                          if (bets[index] > 0) {
+                                            bets[index] -= 1;
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ]),
+                              );
+                            },
+                            separatorBuilder: (BuildContext context, int index) =>
+                            const Divider(),
+                          )),
+                      ElevatedButton(
+                        style: mainButton,
                           onPressed: () {
-                            setState(() {
-                              if (bets[index] > 0) {
-                                bets[index] -= 1;
-                              }
-                            });
+                            //update player bet info in presenter
+                            Navigator.pop(context);
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                                  return EnterResultsView();
+                                }));
                           },
-                        ),
-                      ),
-                    ]),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
-              )),
-              ElevatedButton(
-                  onPressed: () {
-                    //update player bet info in presenter
-                    Navigator.pop(context);
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return EnterResultsView();
-                    }));
-                  },
-                  child: const Text('Submit Bets'))
-            ],
-          )),
-    );
+                          child: const Text('Submit Bets')),
+                    ],
+                )),
+            )
+          )
+      ));
   }
 }
