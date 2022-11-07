4556e809e6898cbf8fbacade47cdbc3b1f8f7d6f
Ignacio R. de Almeida <57123713+IgnacioAlmeida@users.noreply.github.com>
2022-11-06 21:36:55 -0700

    Center and add background img in startingRound View

--- lib/views/startingRound.view.dart
+++ lib/views/startingRound.view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skull_king_scorekeeper/utils/buttons.dart';
 import 'package:skull_king_scorekeeper/views/betting.view.dart';
 import 'package:delayed_display/delayed_display.dart';
 
@@ -13,54 +14,68 @@ class _StartingRoundViewState extends State<StartingRoundView> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text('Starting Round...')),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const Text(
-                'Get ready for round... 1!',
-                style: TextStyle(fontSize: 30),
-                textAlign: TextAlign.center,
-              ),
-              DelayedDisplay(
-                delay: initialDelay,
-                child: const Text(
-                  'YO',
-                  style: TextStyle(fontSize: 64),
-                  textAlign: TextAlign.center,
-                ),
-              ),
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  const Text(
+                    'Get ready for round... 1!',
+                    style: TextStyle(fontSize: 30),
+                    textAlign: TextAlign.center,
+                  ),
+                  DelayedDisplay(
+                    delay: initialDelay,
+                    child: const Text(
+                      'YO',
+                      style: TextStyle(fontSize: 64),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
 
-              DelayedDisplay(
-                delay:
+                  DelayedDisplay(
+                    delay:
                     Duration(milliseconds: initialDelay.inMilliseconds + 700),
-                child: const Text(
-                  'HO',
-                  style: TextStyle(fontSize: 64),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              DelayedDisplay(
-                delay:
+                    child: const Text(
+                      'HO',
+                      style: TextStyle(fontSize: 64),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  DelayedDisplay(
+                    delay:
                     Duration(milliseconds: initialDelay.inMilliseconds + 1400),
-                child: const Text(
-                  'HO',
-                  style: TextStyle(fontSize: 64),
-                  textAlign: TextAlign.center,
-                ),
+                    child: const Text(
+                      'HO',
+                      style: TextStyle(fontSize: 64),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  // TODO: later we will have an animation here for the countdown, but for right now just click button
+                  ElevatedButton(
+                    style:mainButton,
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                              return BettingView();
+                            }));
+                      },
+                      child: const Text('(Continue)')),
+                  Spacer(),
+                ],
               ),
-              // TODO: later we will have an animation here for the countdown, but for right now just click button
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return BettingView();
-                    }));
-                  },
-                  child: const Text('(Continue)'))
-            ],
-          )),
+            ),
+          )
+        )),
     );
   }
 }
